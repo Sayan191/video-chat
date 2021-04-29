9311dce9e6e2257d87a6e10a1d692c255168b44b
Sayan Talukdar <53225067+Sayan191@users.noreply.github.com>
2021-04-29 10:06:16 +0530

    make it better

---
+++ index.js
@@ -0,0 +1,39 @@
+const app = require('express')();
+const server = require('http').createServer(app);
+const cors = require("cors")
+
+const io = require('socket.io')(server,{
+    cors:{
+        origin: "*",
+        method: ["GET","POST"],
+    }
+});
+
+app.use(cors());
+
+app.get("/", (req,res)=>{
+    res.send("SERVER is UP......");
+});
+
+
+//connection
+io.on("connection", (socket)=>{
+    socket.emit("me",socket.id);
+
+    socket.on("disconnect",()=>{
+        socket.broadcast.emit("callEnded")
+    });
+
+    socket.on("callUser",({userToCall, signalData, from, name})=>{
+        io.to(userToCall).emit("callUser",{signal:signalData, from, name});
+    })
+
+    socket.on("answerCall",(data)=>{
+        io.to(data.to).emit("CallAccepted", data.signal)
+    })
+});
+
+const PORT = process.env.PORT || 5000;
+server.listen(PORT, ()=>{
+    console.log(`Connected to PORT ${PORT}`)
+});
